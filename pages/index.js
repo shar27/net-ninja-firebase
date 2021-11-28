45159578b205eb2cb01c08442ab1e28a1631e35a
shar27 <shariq.ahmed731@gmail.com>
2021-11-28 11:19:07 +0100

    firebase

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Firebase app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Firebase</h1>
+      
+    </div>
+  )
+}
